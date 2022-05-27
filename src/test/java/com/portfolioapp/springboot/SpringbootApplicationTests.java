2864b89eb7ffc53dc83b4735962fc21a0c8703e6
tobsailbot <48391987+tobsailbot@users.noreply.github.com>
2022-05-27 11:55:31 -0300

    login implemented

---
+++ src/test/java/com/portfolioapp/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolioapp.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
